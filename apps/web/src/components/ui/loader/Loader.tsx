af12bbf17c077d75f2cc84510c60cdb5f4fb744e
hadrien <smethadrien.96@gmail.com>
2025-07-11 20:15:19 +0200

    same

--- apps/web/src/components/ui/loader/Loader.tsx
+++ apps/web/src/components/ui/loader/Loader.tsx
@@ -1,15 +1,11 @@
-import { AnimationProps } from "@/assets";
-
 import "./loader.scss";
 
 export const LoaderSizes = {
     fullScreen: 200,
     small: 80,
 } as const;
-export const Loader = (props: AnimationProps & { size?: keyof typeof LoaderSizes }) => {
-    const style = props.size
-        ? { maxWidth: LoaderSizes[props.size], ...props.style }
-        : props.style;
+export const Loader = ({ size = "small" }: { size?: keyof typeof LoaderSizes }) => {
+    const style = { maxWidth: LoaderSizes[size] };
 
     return (
         <div className="loader-container">
