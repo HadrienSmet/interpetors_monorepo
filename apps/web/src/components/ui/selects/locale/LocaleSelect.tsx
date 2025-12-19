96ab9a12fc746628441416122ca038eebbf2bf92
hadrien <106483618+HadrienSmet@users.noreply.github.com>
2025-12-19 13:41:41 +0100

    Fixing locale select default value

--- apps/web/src/components/ui/selects/locale/LocaleSelect.tsx
+++ apps/web/src/components/ui/selects/locale/LocaleSelect.tsx
@@ -6,13 +6,14 @@ import { SUPPORTED_LANGUAGES } from "@/i18n/languages";
 import { Select } from "../core";
 
 export const LocaleSelect = () => {
-    const { setLocale } = useLocale();
+    const { locale, setLocale } = useLocale();
 
     const onChange = (event: ChangeEvent<HTMLSelectElement>) =>
         setLocale(event.target.value);
 
     return (
         <Select
+            defaultValue={locale}
             name="locale"
             onChange={onChange}
             options={Object.keys(SUPPORTED_LANGUAGES).map(lang => ({
